337e4e3d56c66fa667f034ccd27c84fdaadc686c
tfnLAB <tfnlab@gmail.com>
2022-08-08 19:36:49 -0700

    first upload

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -4,11 +4,10 @@ const AccountDetails = ({ accountAddress, accountBalance }) => {
   return (
     <div>
       <div className="jumbotron">
-        <h1 className="display-5">Welcome to TFNLab</h1>
+        <h1 className="display-5">Welcome</h1>
         <div class="card col-md-12" >
           <div class="card-body">
-            <h4>Non-Fungilbe Token Lab</h4>
-            <p className="lead">TFNLab creates art products, services and experiences</p>
+            <h4>We make art NFT collectables</h4>
           </div>
         </div>
       </div>
