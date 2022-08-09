679dd8d6f1c20231dfceaa8fb8011a70586ee35e
tfnLAB <tfnlab@gmail.com>
2022-08-09 10:39:47 -0700

    first upload

--- src/components/AccountDetails/AccountDetails.jsx
+++ src/components/AccountDetails/AccountDetails.jsx
@@ -8,7 +8,7 @@ const AccountDetails = ({ accountAddress, accountBalance }) => {
         <div class="card col-md-12" >
           <div class="card-body">
             <h4>We make art NFT collectables</h4>
-            <img src="images/parts/body/0.png" style={{ height: '80%', width: '80%'}}/>
+            <img src="images/parts/body/0.png" style={{ height: '100%', width: '100%'}}/>
           </div>
         </div>
       </div>
