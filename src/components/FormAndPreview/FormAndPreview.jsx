3cbc0952cb866eafe6ed09430000cdb56cf7e91c
tfnLAB <tfnlab@gmail.com>
2022-08-08 19:02:45 -0700

    first upload

--- src/components/FormAndPreview/FormAndPreview.jsx
+++ src/components/FormAndPreview/FormAndPreview.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import CryptoBoyNFTImage from "../CryptoBoyNFTImage/CryptoBoyNFTImage";
 import queryString from 'query-string'
 import { HashRouter } from 'react-router-dom'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
