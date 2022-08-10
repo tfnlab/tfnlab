857f00680ca8b882ed64becb0bb99d0dc8347e0d
tfnLAB <tfnlab@gmail.com>
2022-08-10 08:38:17 -0700

    update

--- src/components/ConnectMetamask/ConnectToMetamask.jsx
+++ src/components/ConnectMetamask/ConnectToMetamask.jsx
@@ -9,10 +9,10 @@ const ConnectToMetamask = ({ connectToMetamask }) => {
         <h1 className="display-5">
           TFNLab
         </h1>
-        <p>Own a piece of digital art that is completely unique and one of a kind.</p>
         <hr className="my-4" />
+        <p>Own a piece of digital art that is completely unique and one of a kind.
         <a href="https://play.tfnlab.com" >Play.tfnlab.com</a>
-        <p>Download a print-ready high-resolution 1500 x 1500 resolution image</p>
+        Download a print-ready high-resolution 1500 x 1500 resolution image</p>
         <hr className="my-4" />
         <a href="https://opensea.io/collection/tfnlab" ><img class="img-fluid" src="/images/main.logo.gif" style={{ height: '100%', width: '100%'}} /></a>
 
